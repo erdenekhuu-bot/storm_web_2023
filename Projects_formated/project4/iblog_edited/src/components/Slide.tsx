1df091b6f7675fe02be66bb056eacd6e32ce809c
Storm-12 <storm-12@Admin-12s-iMac.local>
2023-11-28 15:50:00 +0800

    changed

--- Projects_formated/project4/iblog_edited/src/components/Slide.tsx
+++ Projects_formated/project4/iblog_edited/src/components/Slide.tsx
@@ -2,42 +2,31 @@ import title from '../assets/title_icon.png'
 import '../App.css'
 import frame from '../assets/Frame.png'
 import { useState } from 'react'
+import {Menu1, Menu2}from './Menu'
 
 function SlideBar(){
-    const [state, setState]=useState(0);
-    
+    const [isOpen, setIsOpen]=useState(false);
+    function trigger(){
+        setIsOpen(!isOpen);
+    }
     return (
         <div className="slide-bar">
             <div id='title'>
                 <img src={title} alt="" /> <span className='i'>i</span><span className='blog'>blog</span> <p></p>
-                    <div className='slide-box' onClick={function(){
-                        const result = state == 0 ? 1 : 0;
-                        console.log(result)
-                    }}><b>Lifestyle</b> <span><img src={frame} alt="" /></span>
-                        <div className='sub-slide'>Hobby</div>
-                        <div className='sub-slide'>Travel</div>
-                        <div className='sub-slide'>Food&Beverage</div>
-                        <div className='sub-slide'>Home and decor</div>
+                    <div className='slide-box' onClick={trigger}>
+                        <b>Lifestyle</b> <span><img src={frame} alt="" /></span>
+                        {isOpen && <Menu1 txt1='Hobby' txt2='Travel' txt3='Food&Beverage' txt4='Home and decor'/>}
                     </div>
 
-                    <div className='slide-box' onClick={function(){
-                        const result = state == 0 ? 1 : 0;
-                        console.log(result)
-                    }}><b>Sport</b> <span><img src={frame} alt="" /></span>
-                        <div className='sub-slide'>Soccer</div>
-                        <div className='sub-slide'>Volleyball</div>
+                    <div className='slide-box' onClick={trigger}>
+                        <b>Sport</b> <span><img src={frame} alt="" /></span>
+                        {/* {isOpen && <Menu2 txt1='Soccer' txt2='Volleyball'/>} */}
                     </div>
 
-                    <div className='slide-box' onClick={function(){
-                        const result = state == 0 ? 1 : 0;
-                        console.log(result)
-                    }}><b>Technology</b><span><img src={frame} alt="" /></span>
+                    {/* <div className='slide-box'><b>Technology</b><span><img src={frame} alt="" /></span>
                     </div>
-                    <div className='slide-box' onClick={function(){
-                        const result = state == 0 ? 1: 0;
-                        console.log(result)
-                    }}><b>Entertainment</b><span><img src={frame} alt="" /></span>
-                    </div>
+                    <div className='slide-box'><b>Entertainment</b><span><img src={frame} alt="" /></span>
+                    </div> */}
             </div>
             
         </div>
